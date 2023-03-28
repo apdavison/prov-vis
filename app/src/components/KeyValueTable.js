8e8e695ba27e81d0015c6e935a424de7e0262860
Andrew Davison <andrew.davison@cnrs.fr>
2023-03-28 18:04:44 +0200

    improve <KeyValueTable> component

--- app/src/components/KeyValueTable.js
+++ app/src/components/KeyValueTable.js
@@ -1,44 +1,47 @@
-
-
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableRow,
+  TableContainer,
+  TableCell,
+} from "@mui/material";
 
 function KeyValueTable(props) {
+  let formatKey = (key) => {
+    return key;
+  };
+  if (props.boldKeys) {
+    formatKey = (key) => {
+      return <b>{key}</b>;
+    };
+  }
 
-    let formatKey = (key) => {
-        return key
+  let rows = [];
+  console.log(props.data);
+  if (props.data) {
+    for (const [key, value] of Object.entries(props.data)) {
+      let valueStr = String(value);
+      if (Array.isArray(value)) {
+        valueStr = value.join(", ");
+      }
+
+      rows.push(
+        <TableRow key={key} sx={{ "&:last-child td": { border: 0 } }}>
+          <TableCell>{formatKey(key)}</TableCell>
+          <TableCell>{valueStr}</TableCell>
+        </TableRow>
+      );
     }
-    if (props.boldKeys) {
-        formatKey = (key) => {
-            return <b>{key}</b>
-        }
-    }
+  }
 
-    let rows = [];
-    console.log(props.data);
-    if (props.data) {
-        for (const [key, value] of Object.entries(props.data)) {
-            let valueStr = String(value);
-            if (Array.isArray(value)) {
-                valueStr = value.join(", ")
-            };
-
-            rows.push(
-                <tr key={key}>
-                    <td>{formatKey(key)}</td>
-                    <td>{valueStr}</td>
-                    </tr>
-            )
-        }
-    }
-
-    return (
-        <table>
-            <tbody>
-                {rows}
-            </tbody>
-        </table>
-    )
-
-
+  return (
+    <TableContainer component={Paper} sx={{width: "max-content"}}>
+      <Table size="small" aria-label="key-value table">
+        <TableBody>{rows}</TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export default KeyValueTable;
