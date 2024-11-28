ac6271ab0b249c9a57dc897c2c045f24aade4518
Andrew Davison <andrew.davison@cnrs.fr>
2024-11-28 18:30:32 +0100

    update API URL

--- app/src/globals.js
+++ app/src/globals.js
@@ -1,6 +1,6 @@
 const JITTER = 20;
 const USE_TEST_DATA = false;
-const baseUrl = "https://prov.brainsimulation.eu";
+const baseUrl = "https://prov-api.apps.tc.humanbrainproject.eu";
 
 export {
     JITTER, USE_TEST_DATA, baseUrl
